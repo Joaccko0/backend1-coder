cf8ae3f52937d4aa8dd7014266e4629da4bfdef3
Joaccko0 <joaquinrd0@gmail.com>
2025-04-28 01:57:54 -0300

    2da entrega en curso

--- app.js
+++ app.js
@@ -2,19 +2,32 @@
 
 // app.js
 const express = require('express');
+const { engine } = require('express-handlebars')
 const app = express();
-const PORT = 8080;
+const PORT = 3000;
 
 const productsRouter = require('./routes/products');
 const cartsRouter = require('./routes/carts');
+
+app.engine('handlebars', engine({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+const http = require('http');
+const server = http.createServer(app);
+const io = require('socket.io')(server);
 
 app.use(express.json()); // Para poder trabajar con JSON en el body
 app.use('/api/products', productsRouter);
 app.use('/api/carts', cartsRouter);
 
-app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
-  });
+// Escuchar conexiones de cliente
+io.on('connection', (socket) => {
+  console.log('Nuevo cliente conectado');
+});
 
-// Cada grupo de rutas (/api/products y /api/carts) va a ser manejado por su propio archivo router, que se 
-// encargará de delegar las tareas al manager correspondiente.
+// Arrancar servidor en el mismo puerto
+server.listen(3000, () => {
+  console.log('Servidor corriendo en http://localhost:3000');
+});
+
+module.exports = { server, io };
